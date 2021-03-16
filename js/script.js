472a671c6072ff7b025c64856ee3641a877e579f
Alternative WebCloud <75317111+alternative1webcloud@users.noreply.github.com>
2021-03-16 22:06:25 +0300

    slider and grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+import Swiper from 'swiper/bundle'
 
+const swiper = new Swiper('.swiper__container', {
+    direction: 'horizontal',
+    loop: true,
+    scrollbar: {
+        el: '.swiper__scrollbar'
+    }
+})
